4774edd22538f4ddb73f68a4ed5f49c37b4818a2
summwer <15091002@buaa.edu.cn>
2024-02-14 14:46:55 +0800

    update implement_lwe_instance

--- implement_lwe_instance.sh
+++ implement_lwe_instance.sh
@@ -9,18 +9,18 @@ cd ..
 cd ..
 
 
-for j in $(seq 5 6) #do
+for j in $(seq 1 4) #do
 do
 
-    # for i in $(seq 49 49) 
-    # do
+    for i in $(seq 48 52) 
+    do
 
     # # # # ###########################################
     # # # # # BSSA strategy test: d4f-default-g6k
     # # # # #
     # # # # ###########################################
 
-    # python ProPnjBKZ_for_lwe.py ${i} --lwe/alpha 0.015 --gpus 2 --threads 32 --verbose True --pump/down_sieve True  --strategy_method "bssav1" --load_lwe "lwe_instance" --float_type "dd" | tee "lwechal-instance-test-dd"/"d4f-default-g6k"/"bssa(32+2gpus)"/${i}-015-${j}.log
+    python ProPnjBKZ_for_lwe.py ${i} --lwe/alpha 0.015 --gpus 2 --threads 32 --verbose True --pump/down_sieve True  --strategy_method "bssav1" --load_lwe "lwe_instance" --float_type "dd" | tee "lwechal-instance-test-dd"/"d4f-default-g6k"/"bssa(32+2gpus)"/${i}-015-${j}.log
 
 
     # # # ###########################################
@@ -28,16 +28,16 @@ do
     # # # #
     # # # ###########################################
 
-    # python ProPnjBKZ_for_lwe.py ${i} --lwe/alpha 0.015 --gpus 2 --threads 32 --verbose True --pump/down_sieve True  --strategy_method "enumbs" --load_lwe "lwe_instance" --float_type "dd"| tee "lwechal-instance-test-dd"/"d4f-default-g6k"/"enumbs(32+2gpus)"/${i}-015-${j}.log
+    python ProPnjBKZ_for_lwe.py ${i} --lwe/alpha 0.015 --gpus 2 --threads 32 --verbose True --pump/down_sieve True  --strategy_method "enumbs" --load_lwe "lwe_instance" --float_type "dd"| tee "lwechal-instance-test-dd"/"d4f-default-g6k"/"enumbs(32+2gpus)"/${i}-015-${j}.log
 
     # # ###########################################
     # # # LWE instance solved by default G6K test
     # # #
     # # ###########################################
 
-    # python lwe_instance.py ${i} --lwe/alpha 0.015 --threads 32 --gpus 2 --verbose True --pump/down_sieve True  | tee "lwechal-instance-test-dd"/"default_g6k_main(32+2gpus)"/${i}-015-${j}.log
+    python lwe_instance.py ${i} --lwe/alpha 0.015 --threads 32 --gpus 2 --verbose True --pump/down_sieve True  | tee "lwechal-instance-test-dd"/"default_g6k_main(32+2gpus)"/${i}-015-${j}.log
 
-    # done
+    done
 
     ###########################################
     # LWE instance solved by default G6K test
@@ -45,10 +45,10 @@ do
     ###########################################
 
 
-    for i in $(seq 56 56) #do
+    for i in $(seq 54 61) #do
     do
     
-    # python lwe_instance.py $i --lwe/alpha 0.010 --threads 32 --gpus 2 --verbose True --pump/down_sieve True  | tee "lwechal-instance-test-dd"/"default_g6k_main(32+2gpus)"/$i-010-${j}.log
+    python lwe_instance.py $i --lwe/alpha 0.010 --threads 32 --gpus 2 --verbose True --pump/down_sieve True  | tee "lwechal-instance-test-dd"/"default_g6k_main(32+2gpus)"/$i-010-${j}.log
 
     # # ###########################################
     # # # EnumBS strategy test:  d4f-default-g6k
@@ -62,7 +62,7 @@ do
     # # # #
     # # # ###########################################
 
-    # python ProPnjBKZ_for_lwe.py ${i} --lwe/alpha 0.010 --gpus 2 --threads 32 --verbose True --pump/down_sieve True  --strategy_method "bssav1" --load_lwe "lwe_instance" --float_type "dd"| tee "lwechal-instance-test-dd"/"d4f-default-g6k"/"bssa(32+2gpus)"/${i}-010-${j}.log
+    python ProPnjBKZ_for_lwe.py ${i} --lwe/alpha 0.010 --gpus 2 --threads 32 --verbose True --pump/down_sieve True  --strategy_method "bssav1" --load_lwe "lwe_instance" --float_type "dd"| tee "lwechal-instance-test-dd"/"d4f-default-g6k"/"bssa(32+2gpus)"/${i}-010-${j}.log
 
     done
 
